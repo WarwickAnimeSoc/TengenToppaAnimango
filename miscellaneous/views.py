441eb8d98c0b462036cdddb51ff130b7c44716d3
Tomas Ituralde <Tomasiturralde123@gmail.com>
2020-09-16 18:04:05 +0100

    Added github redirect

--- miscellaneous/views.py
+++ miscellaneous/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, 'miscellaneous/home.html')
 
+
 def privacy(request):
     return render(request, 'miscellaneous/privacy.html')
 
@@ -12,19 +13,24 @@ def privacy(request):
 
 def discord(request):
     # Redirects to the society Discord server
-    return redirect("https://discordapp.com/invite/GYM6ay7")
+    return redirect('https://discordapp.com/invite/GYM6ay7')
 
 
 def facebook(request):
     # Redirects to the society Facebook group
-    return redirect("https://facebook.com/groups/warwickanimesoc")
+    return redirect('https://facebook.com/groups/warwickanimesoc')
 
 
 def warwicksu(request):
     # Redirects to the Warwick Students Union page for the society
-    return redirect("https://warwicksu.com/societies-sports/societies/animeandmanga")
+    return redirect('https://warwicksu.com/societies-sports/societies/animeandmanga')
 
 
 def malclub(request):
     # Redirects to the MyAnimeList Club for the society
-    return redirect("https://myanimelist.net/clubs.php?cid=78196")
+    return redirect('https://myanimelist.net/clubs.php?cid=78196')
+
+
+def github(request):
+    # Redirects to the Github repo for the site
+    return redirect('https://github.com/Danalite/TengenToppaAnimango')
