329cb707a8f566ba3242b37f3ba697129530beb3
Tomas Ituralde <Tomasiturralde123@gmail.com>
2020-09-18 06:09:25 +0100

    Restyled error pages and changed the function name for the archive size function
    archiveSize -> archiveSizeCheck for clarity.
    Styled error pages for errors 400, 403, 404 and 500.

--- static_files/js/ttam.js
+++ static_files/js/ttam.js
@@ -2,7 +2,7 @@
 
 /* Archive image resize detection */
 /* This function is used in the archive app to check if an image has been resized. */
-function archiveSize() {
+function archiveSizeCheck() {
     let image = document.getElementById('archiveImage');
     if (image) {
         /* If image was null then archive item was a video. Video's can be viewed fullscreen via the controls, so
@@ -16,4 +16,12 @@ function archiveSize() {
             messagesDiv.appendChild(resizeMessage);
         }
     }
+}
+
+/* Error page divergence meter */
+/* This function will generate a divergence meter string in the format 0.XXXXXX, it's used in the error pages. */
+function writeDivergenceMeter() {
+    let divergenceMeter = document.getElementById('divergenceMeter');
+    let divergenceNumber = Math.random();
+    divergenceMeter.innerText = divergenceNumber.toString().slice(0, 8);
 }
