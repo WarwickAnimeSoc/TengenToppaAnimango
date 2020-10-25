a7fec34a3ea49a1d8b8688a597a437a41b529e90
Tomas Ituralde <Tomasiturralde123@gmail.com>
2020-10-25 17:07:19 +0000

    Improved the User admin

--- apps/members/admin.py
+++ apps/members/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Member
+
+
+class MemberInline(admin.StackedInline):
+    model = Member
+    can_delete = False
+
+
+class OverwriteUserAdmin(UserAdmin):
+    inlines = (
+        MemberInline,
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, OverwriteUserAdmin)
