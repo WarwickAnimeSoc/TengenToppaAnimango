6b73cd07dd34dc5de0b312b6c645c53ffc276178
Tomas Iturralde <Tomasiturralde123@gmail.com>
2021-10-03 00:37:18 +0100

    Fixed password reset

--- apps/members/urls.py
+++ apps/members/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     # Only using default auth view for password resets as the others required overwrites.
     path('reset_password/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='members/templates/members/reset_password.html',
+         auth_views.PasswordResetConfirmView.as_view(template_name='members/reset_password.html',
                                                      form_class=OverrideSetPasswordForm,
                                                      success_url=reverse_lazy('members:password_reset_redirect')
                                                      ),
