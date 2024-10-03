1e60baa95c3cf714bd02327468e9614fea3dcfd7
Breadifies <jomds222@gmail.com>
2024-10-03 23:51:25 +0100

    Revert to normal site
    Removed ww event site and relevant images

--- apps/miscellaneous/urls.py
+++ apps/miscellaneous/urls.py
@@ -5,15 +5,13 @@ from . import views
 app_name = 'miscellaneous'
 
 urlpatterns = [
-    path('home', views.home, name='home'), #remember to change this to default site ''
+    path('', views.home, name='home'),
     path('privacy/', views.privacy, name='privacy'),
     path('discord', views.discord, name='discord'),
     path('instagram', views.instagram, name='instagram'),
     path('warwicksu', views.warwicksu, name='warwicksu'),
     path('malclub', views.malclub, name='malclub'),
     path('github', views.github, name='github'),
-    path('klist', views.karaoke_list, name='karaoke_list'),
-    path('', views.welcomeweek, name='welcomeweek2024'), #get rid of this and below after ww
-    path('welcomeweek2024', views.welcomeweek, name='welcomeweek2024')
+    path('klist', views.karaoke_list, name='karaoke_list')
 ]
 
