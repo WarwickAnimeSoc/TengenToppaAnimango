06901cd6e0bcbb4a4935522db6cb2a0b96f9bd89
Octowhat1 <benjermin0987@gmail.com>
2025-09-27 16:18:40 +0100

    Welcome Week 2025

--- apps/miscellaneous/urls.py
+++ apps/miscellaneous/urls.py
@@ -5,13 +5,15 @@ from . import views
 app_name = 'miscellaneous'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'), #REMEMBER TO CHANGE this back to default site - i.e. ''
     path('privacy/', views.privacy, name='privacy'),
     path('discord', views.discord, name='discord'),
     path('instagram', views.instagram, name='instagram'),
     path('warwicksu', views.warwicksu, name='warwicksu'),
     path('malclub', views.malclub, name='malclub'),
     path('github', views.github, name='github'),
-    path('klist', views.karaoke_list, name='karaoke_list')
+    path('klist', views.karaoke_list, name='karaoke_list'),
+    path('', views.welcomeweek, name='welcomeweek2025'), #REMOVE this and below after Welcome Week
+    path('welcomeweek2025', views.welcomeweek, name='welcomeweek2025')
 ]
 
