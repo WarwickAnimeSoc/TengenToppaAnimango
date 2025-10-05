9a72b662e3607d14e0d03f107a049c0651cf2af4
Octowhat1 <benjermin0987@gmail.com>
2025-10-05 17:32:06 +0100

    Reversion back to normal site

--- apps/miscellaneous/urls.py
+++ apps/miscellaneous/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'miscellaneous'
 
 urlpatterns = [
-    path('home', views.home, name='home'), #REMEMBER TO CHANGE this back to default site - i.e. ''
+    path('', views.home, name='home'),
     path('privacy/', views.privacy, name='privacy'),
     path('discord', views.discord, name='discord'),
     path('instagram', views.instagram, name='instagram'),
@@ -13,7 +13,5 @@ urlpatterns = [
     path('malclub', views.malclub, name='malclub'),
     path('github', views.github, name='github'),
     path('klist', views.karaoke_list, name='karaoke_list'),
-    path('', views.welcomeweek, name='welcomeweek2025'), #REMOVE this and below after Welcome Week
-    path('welcomeweek2025', views.welcomeweek, name='welcomeweek2025')
 ]
 
